19bc8291a5cf16e04ad5fbdc9a387667fff4da96
hexavoyager <hexavoyager@gmail.com>
2021-04-19 14:54:55 +0200

    SSaturnReturn website uploadaturnReturn website upload

---
+++ mapscript.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 78.24898954334529, lng: 15.467975818797676 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+    });
+}
